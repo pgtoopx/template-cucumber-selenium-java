fd00f4ac607df8f9f176498b331cb03e8969a895
pgtoopx <eduardoinbrand@gmail.com>
2019-04-22 15:15:33 -0400

    web driver factory fixed, allow default run with chrome

--- src/test/java/cucumber/util/WebDriverFactory.java
+++ src/test/java/cucumber/util/WebDriverFactory.java
@@ -4,10 +4,14 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class WebDriverFactory {
+
+
+
     public static WebDriver createWebDriver() {
-        String webdriver = System.getProperty("browser");
+        String webdriver = System.getProperty("browser", "chrome");
         switch(webdriver) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
@@ -15,6 +19,10 @@ public class WebDriverFactory {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
                 return new ChromeDriver();
+            case "safari":
+                //TODO: implement SafariDriver to factory
+                //return new SafariDriver();
+                throw new RuntimeException("Unsupported webdriver: " + webdriver);
             default:
                 throw new RuntimeException("Unsupported webdriver: " + webdriver);
         }
