4c17cd9fe21f1dac7477b2f836266eb47ed55328
pgtoopx <eduardoinbrand@gmail.com>
2019-04-22 11:17:42 -0400

    implemented hooks that allow manage driver

---
+++ src/test/java/cucumber/Hooks.java
@@ -0,0 +1,43 @@
+package cucumber;
+
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Hooks {
+
+    public static WebDriver driver;
+    @Before
+    /**
+     * Delete all cookies at the start of each scenario to avoid
+     * shared state between tests
+     */
+    public void openBrowser(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+    }
+
+    @After
+    /**
+     * Embed a screenshot in test report if test is marked as failed
+     */
+    public void tearDown(Scenario scenario){
+        if(scenario.isFailed()) {
+            try {
+                scenario.write("Current Page URL is " + driver.getCurrentUrl());
+                byte[] screenshot = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+                scenario.embed(screenshot, "image/png");
+            } catch (WebDriverException somePlatformsDontSupportScreenshots) {
+                System.err.println(somePlatformsDontSupportScreenshots.getMessage());
+            }
+        }
+        driver.quit();
+    }
+
+}
--- src/test/java/cucumber/exampleDefinition/exampleDefinition.java
+++ src/test/java/cucumber/exampleDefinition/exampleDefinition.java
@@ -1,13 +1,23 @@
 package cucumber.exampleDefinition;
 
+import cucumber.Hooks;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
 
 public class exampleDefinition {
+    WebDriver browser;
+    private String URL_GOOGLE = "http://www.google.com";
+    private WebDriver driver;
+
+    public exampleDefinition(){
+        driver= Hooks.driver;
+    }
+
     @Given("i am on google search page")
     public void i_am_on_google_search_page() {
-        // Write code here that turns the phrase above into concrete actions
+        driver.get(URL_GOOGLE);
     }
 
     @When("i search for {string}")
