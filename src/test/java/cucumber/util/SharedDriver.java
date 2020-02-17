552c153edf74b1c54c16de2e7edc79162a9b619f
pgtoopx <eduardoinbrand@gmail.com>
2020-02-17 16:57:41 -0300

    aa

--- src/test/java/cucumber/util/SharedDriver.java
+++ src/test/java/cucumber/util/SharedDriver.java
@@ -1,11 +1,5 @@
 package cucumber.util;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 import java.net.MalformedURLException;
 
 public class SharedDriver {
